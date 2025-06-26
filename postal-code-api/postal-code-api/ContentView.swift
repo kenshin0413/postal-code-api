ef1a53f0212df16e1166f1e1a9a5c6725daee1f4
kenshin0413 <kenshin1234569@gmail.com>
2025-06-26 20:45:01 +0900

    初期

---
+++ postal-code-api/postal-code-api/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  postal-code-api
+//
+//  Created by miyamotokenshin on R 7/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ postal-code-api/postal-code-api/postal_code_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  postal_code_apiApp.swift
+//  postal-code-api
+//
+//  Created by miyamotokenshin on R 7/06/26.
+//
+
+import SwiftUI
+
+@main
+struct postal_code_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
